556959982cc012ebaa5e7bea5ea42d5eeb62301a
Dormouse Young <dormouse.young@gmail.com>
2014-10-11 11:11:21 +0800

    change to bb9800

--- legohole/settings.py
+++ legohole/settings.py
@@ -14,4 +14,23 @@ SPIDER_MODULES = ['legohole.spiders']
 NEWSPIDER_MODULE = 'legohole.spiders'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'legohole (+http://www.yourdomain.com)'
+#BOT_NAME = 'manta'
+ 
+DOWNLOAD_TIMEOUT = 15
 DOWNLOAD_DELAY = 2
+COOKIES_ENABLED = True
+COOKIES_DEBUG = True
+RETRY_ENABLED = False
+ 
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.97 Safari/537.22 AlexaToolbar/alxg-3.1"
+ 
+DEFAULT_REQUEST_HEADERS={
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en',
+    'X-JAVASCRIPT-ENABLED': 'true',
+}
+ 
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': 400,
+    'scrapy.contrib.downloadermiddleware.cookies.CookiesMiddleware':700,
+}
--- legohole/spiders/bb9800.py
+++ legohole/spiders/bb9800.py
@@ -4,19 +4,37 @@ import scrapy
 
 class Bb9800Spider(scrapy.Spider):
     name = "bb9800"
-    allowed_domains = ["www.nphoto.net"]
+    allowed_domains = ["bb9800.diandian.com"]
     start_urls = (
-        'http://www.nphoto.net/news/',
+        'http://bb9800.diandian.com/',
     )
+    def _get_text(self, res, path):
+        strings = res.xpath(path).extract()
+        return ''.join(strings).strip()
 
     def parse(self, response):
-        articles = response.xpath('//div[@class="article"]')
-        for article in articles:
-            link = article.xpath('table/tr/td/a/@href')[0].extract()
-            title = article.xpath('table//span/text()')[0].extract()
-            print title
-            print link
-            yield scrapy.Request(link, callback=self.parse_single_page)
+        #get all posts
+        posts = response.xpath('//div[starts-with(@class,"post")]')
+        for post in posts:
+            h1 = post.xpath('div[@class="entry"]/h1')
+            if len(h1) == 1:
+                #we have title!
+                title = self._get_text(h1, 'a/text()')
+                link = self._get_text(h1, 'a/@href')
+                print title
+                print link
+            else:
+                #we have no title
+                #get date
+                day = self._get_text(post, 'div[@class="date"]/span/text()')
+                month = self._get_text(post, 'div[@class="date"]/text()')
+                print day
+                print month
+
+
+
+            #for entry in entries:
+                #yield scrapy.Request(link, callback=self.parse_single_page)
 
     def parse_single_page(self, response):
         title = response.xpath('//h1/text()').extract()
