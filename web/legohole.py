e50d02bd05582d1da34132b564493a6d7835376d
Dormouse Young <dormouse.young@gmail.com>
2015-03-20 21:49:39 +0800

    update template and legohole.py

--- web/legohole.py
+++ web/legohole.py
@@ -36,7 +36,7 @@ def index():
     return render_template('index.html', sets=sets)
 
 @app.route('/set/number/<set_number>')
-def sets(set_number):
+def sets_by_number(set_number):
     number, variant = set_number.split('-')
     sql = ' '.join([
             "select * from",
@@ -45,10 +45,11 @@ def sets(set_number):
             ])
     args = (number, variant)
     row = query_db(sql, args, one=True)
+    print 'us$$$', row['usprice']
     show_items = [{'name':u'名称', 'value':row['name']},
             {'name':u'编号', 'value':row['number']+'-'+row['variant']},
             {'name':u'系列', 'value':row['theme']+'-'+row['subtheme']},
-            {'name':u'价格', 'value':'$'+row['usprice']},
+            {'name':u'价格', 'value':row['usprice']},
             {'name':u'年份', 'value':row['year']},
             {'name':u'人仔', 'value':row['minifigs']},
             ]
@@ -78,10 +79,38 @@ def sets(set_number):
     objs = {}
     objs['show_items'] = show_items
     objs['img_url'] = row['imageurl']
-
-
     return render_template('set_detail.html', objs=objs)
     
+@app.route('/set/id/<set_id>')
+def sets_by_id(set_id):
+    sql = ' '.join([ "select * from", TABLE, "where id=?"])
+    args = (set_id,)
+    row = query_db(sql, args, one=True)
+
+    row['price'] = '|'.join([item for item in [
+        u'$'+row['usprice'] if row['usprice'] else '',
+        u'£'+row['ukprice'] if row['ukprice'] else '',
+        u'€'+row['euprice'] if row['euprice'] else '',
+        ] if item])
+
+    row['price'] = row['price'] if row['price'] else u'无'
+    row['subtheme'] = row['subtheme'] if row['subtheme'] else u'无'
+    row['minifigs'] = row['minifigs'] if row['minifigs'] else u'无'
+
+    show_items = [
+            {'name':u'编号', 'value':row['number']+'-'+row['variant']},
+            {'name':u'名称', 'value':row['name']},
+            {'name':u'系列', 'value':row['theme']},
+            {'name':u'子系列', 'value':row['subtheme']},
+            {'name':u'价格', 'value':row['price']},
+            {'name':u'年份', 'value':row['year']},
+            {'name':u'人仔', 'value':row['minifigs']},
+            ]
+    objs = {}
+    objs['show_items'] = show_items
+    objs['img_url'] = row['imageurl']
+    return render_template('set_detail.html', objs=objs)
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     error = None
@@ -95,7 +124,7 @@ def search():
                     'order by number',
                     ])
             sets = query_db(sql)
-            return render_template('sets.html', sets=sets)
+            return render_template('set_list.html', sets=sets)
 
 if __name__ == '__main__':
     app.run(debug=True)
