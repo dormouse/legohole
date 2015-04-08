adc4a9591c0383d8070f466023464899e473f484
Dormouse Young <dormouse.young@gmail.com>
2015-04-08 21:49:15 +0800

    improve import_csv.py & add some new lego data

--- testdata/import_csv.py
+++ testdata/import_csv.py
@@ -34,9 +34,11 @@ def init_db():
 
 def import_data():
     cx = sqlite.connect("test.db")
-    for file in glob.glob('*.csv'):
-        if file != 'brickset_20150319_date-added.csv':
-            pro_data(cx, file)
+    #for file in glob.glob('*.csv'):
+    #    if file != 'brickset_20150319_date-added.csv':
+    #        pro_data(cx, file)
+    file = 'brickset_20150408_date-added.csv'
+    pro_data(cx, file)
     cx.commit()
     cx.close()
 
@@ -45,32 +47,55 @@ def pro_data(cx, filename):
         reader = csv.DictReader(f)
         try:
             for row in reader:
-                datas = (row['SetID'], row['Number'], row['Variant'],
-                    row['Theme'], row['Subtheme'], row['Year'], row['Name'],
-                    row['Minifigs'], row['Pieces'], row['UKPrice'],
-                    row['USPrice'], row['CAPrice'], row['EUPrice'],
-                    row['ImageURL'], row['Owned'], row['Wanted'],
-                    row['QtyOwned'])
-                if row['Year'] == "Year":
-                    print filename
-                data_db = [item.decode('utf-8') for item in datas]
-                data_db.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S %f') )
-                data_db.append('brickset')
-                data_clean = (data_db)
-
-                sql = u"insert into brickset (id,setid,number,variant,theme,\
-                    subtheme,year,name,minifigs,pieces,ukprice,\
-                    usprice,caprice,euprice,imageurl,owned,wanted,qtyowned,\
-                    add_time,modi_time,data_source)\
-                    values (null,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,null,?)"
-                cx.execute(sql, data_clean)
-
+                if query_by_setid(row['SetID']):
+                    pass
+                else:
+                    write_db(cx, row)
         except csv.Error as e:
             sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
 
-def test():
-    print glob.glob('*.csv')
+def write_db(cx, row):
+    datas = (row['SetID'], row['Number'], row['Variant'],
+        row['Theme'], row['Subtheme'], row['Year'], row['Name'],
+        row['Minifigs'], row['Pieces'], row['UKPrice'],
+        row['USPrice'], row['CAPrice'], row['EUPrice'],
+        row['ImageURL'], row['Owned'], row['Wanted'],
+        row['QtyOwned'])
+    if row['Year'] == "Year":
+        print filename
+    data_db = [item.decode('utf-8') for item in datas]
+    data_db.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S %f') )
+    data_db.append('brickset')
+    data_clean = (data_db)
+
+    sql = u"insert into brickset (id,setid,number,variant,theme,\
+        subtheme,year,name,minifigs,pieces,ukprice,\
+        usprice,caprice,euprice,imageurl,owned,wanted,qtyowned,\
+        add_time,modi_time,data_source)\
+        values (null,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,null,?)"
+    cx.execute(sql, data_clean)
+
+def query_by_setid(setid):
+    sql = 'select * from brickset where setid = ?'
+    args = (setid,)
+    rvs = query_db(sql, args)
+    found_rows = len(rvs)
+    if found_rows == 1:
+        return True
+    if found_rows == 0:
+        return False
+    if found_rows > 1:
+        print 'error:mulity setid:%s'%(args[0])
+        return False
+
+def query_db(query, args=(), one=False):
+    cx = sqlite.connect("test.db")
+    cur = cx.execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+               for idx, value in enumerate(row)) for row in cur.fetchall()]
+    cx.close()
+    return (rv[0] if rv else None) if one else rv
 
 if __name__ == '__main__':
-    init_db()
     import_data()
+    #print glob.glob('*.csv')
